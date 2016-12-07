f3a15d1f85d4e8b60ec426289615a073e6e06871
lalaheihaihei <liujinchengcheng@hotmail.com>
2016-12-07 16:02:05 +0800

    make the kmc.py clearer

--- SAmodule/kmc.py
+++ SAmodule/kmc.py
@@ -8,6 +8,7 @@ main program to run kMC
 """
 import math
 import random
+import sys
 
 from SAmodule import adsorption
 from SAmodule import reactions
@@ -17,13 +18,45 @@ from SAmodule import lattice
 
 
 def test_kinds_number(kmc):
+    """
+    Test the input species number. Whether the TS + Local minimums = tot species.
+
+    :param kmc: Instance of Parameters
+    :return: None
+    """
     if kmc.isKindsTS.count(0) != len(kmc.kinds) - kmc.isKindsTS.count(1):
         raise ValueError('Error, please checks the kinds and isKindsTS parameters.')
     return None
 
 
-# Add set species class with kind, frequency, and energy information and append them to speciesList.
+def test_reaction_number(kmc):
+    """
+    Test the input reaction number. Whether the n(ads+des+rea) = n(reactions)
+
+    :param kmc: Instance of Parameters
+    :return: None
+    """
+    if len(kmc.reactionsKind) != len(kmc.reactions):
+        raise ValueError('Error: Please check the reactions number and reactionsKind number')
+    return None
+
+
 def add_list(kmc, speciesList = [], TSList = [], totList = []):
+    """
+    Set species class with kind, frequency, and energy information
+    and append them to speciesList, TSList, and totList.
+
+    :param kmc: Instance of Parameters
+    :param speciesList: New List to store the local minimum species information.
+    :param TSList: New List to store the TS species information.
+    :param totList: New List to store the total species information.
+    :return speciesTuple: Tuple to store the local minimum species information.
+    :return speciesTuple_kind: Tuple to store the local minimum species kinds.
+    :return TSTuple: Tuple to store the TS species information.
+    :return totList: Tuple to store the total species information.
+    :return totList_kind: Tuple to store the total species kinds.
+
+    """
     for i in range(len(kmc.kinds)):
         if kmc.isKindsTS[i] == 0:
             speciesList.append(species.species(kmc.kinds[i], kmc.kindsFreq[i], kmc.kindsEnergy[i]))
@@ -33,29 +66,43 @@ def add_list(kmc, speciesList = [], TSList = [], totList = []):
             totList.append(TSList[-1])
         else:
             raise ValueError('Error: Error, please checks the isKindsTS parameters.')
-    # Transfer local minimum list and ts list to tuple format.
     speciesTuple = tuple(speciesList)
     speciesTuple_kind = tuple(i.kind for i in speciesTuple)
     TSTuple = tuple(TSList)
     totList_kind = tuple(i.kind for i in totList)
-    # print('totlist = ', totList)
     return speciesTuple, speciesTuple_kind, TSTuple, totList, totList_kind
 
-# print(speciesTuple, speciesTuple_kind, TSTuple)
-# speciesTuple[8] = speciesTuple[1]
 
-def add_reaction(kmc, totList, T, P, reactionList = [], k_forward = [], k_reverse = [], dict_reaction = {}, dict_reaction_direction = {}):
+def add_reaction(kmc, totList, T, P, reactionList = [], k_forward = [], k_reverse = [],\
+                 dict_reaction = {}, dict_reaction_direction = {}):
+    """
+    Calculation the reaction rate and store reaction informations.
+
+    :param kmc: Instance of Parameters
+    :param totList: Tuple with the total species information.
+    :param T: Temperature
+    :param P: Partial pressure
+    :param reactionList: New List to store all the reaction informations.
+    :param k_forward: New List to store all the forward reaction k.
+    :param k_reverse: New List to store all the reverse reaction k.
+    :param dict_reaction: New dict to store {IS:[k]}, ex:
+        {0: [12.934191, 12.93419, 10.318585], 1: [0.0004196, 0.04606873]}
+    :param dict_reaction_direction: New dict to store {IS:[FS]}, ex:
+        {0: [1, 9, 8], 1: [0, 3], 3: [1, 4], 4: [3, 5, 11], 5: [4, 6]}
+    :return: reactionList, k_forward, k_reverse, dict_reaction, dict_reaction_direction
+    """
     for i in range(len(kmc.reactions)):
+        # judge the reaction kind, and calculate k and G, append it to reactionList.
         if kmc.reactionsKind[i] == 'ads':
             reactionList.append(adsorption.Adsorption\
                 (totList[int(kmc.reactions[i][0][0])], totList[int(kmc.reactions[i][1][0])], T, \
                  P[kmc.reactions[i][0][1].strip()], kmc.reactions[i][0][1].strip()))
             print("For step %d:\tk(ads) = %.3e,\tk(des) = %.3e,\tDeltaG = %.3f" % \
                   (i + 1, reactionList[-1].adsorb_k(), reactionList[-1].desorb_k(), reactionList[-1].delta_g()))
-            f = reactionList[-1].adsorb_k()
-            r = reactionList[-1].desorb_k()
-            k_forward.append(f)  # WHY does work by directly append like k_forward.append(reactionList[-1].adsorbK())
-            k_reverse.append(r)
+            k_forward.append(reactionList[-1].adsorb_k())  # WHY does work by directly append like k_forward.append(reactionList[-1].adsorbK())
+            k_reverse.append(reactionList[-1].desorb_k())
+
+            # if IS in dict_reaction_direction: append(k), if not create IS:[k]
             if int(kmc.reactions[i][0][0]) in dict_reaction.keys():
                 dict_reaction[int(kmc.reactions[i][0][0])].append(k_forward[-1])
             else:
@@ -65,6 +112,7 @@ def add_reaction(kmc, totList, T, P, reactionList = [], k_forward = [], k_revers
             else:
                 dict_reaction[int(kmc.reactions[i][1][0])] = [k_reverse[-1]]
 
+            # if IS in dict_reaction_direction: append(FS), if not create IS:[FS]
             if int(kmc.reactions[i][0][0]) in dict_reaction_direction.keys():
                 dict_reaction_direction[int(kmc.reactions[i][0][0])].append(int(kmc.reactions[i][1][0]))
             else:
@@ -80,10 +128,8 @@ def add_reaction(kmc, totList, T, P, reactionList = [], k_forward = [], k_revers
                      totList[int(kmc.reactions[i][2][0])], T))
             print("For step %d:\tk(forwards) = %.3e,\tk(reverse) = %.3e"\
                   % (i+1, reactionList[-1].forwardK, reactionList[-1].reverseK))
-            f = reactionList[-1].forwardK
-            r = reactionList[-1].reverseK
-            k_forward.append(f)
-            k_reverse.append(r)
+            k_forward.append(reactionList[-1].forwardK)
+            k_reverse.append(reactionList[-1].reverseK)
             if int(kmc.reactions[i][0][0]) in dict_reaction.keys():
                 dict_reaction[int(kmc.reactions[i][0][0])].append(k_forward[-1])
             else:
@@ -108,10 +154,8 @@ def add_reaction(kmc, totList, T, P, reactionList = [], k_forward = [], k_revers
                  P[kmc.reactions[i][1][1].strip()], kmc.reactions[i][1][1].strip()))
             print("For step %d:\tk(des) = %.3e,\tk(ads) = %.3e,\tDeltaG = %.3f" % \
                   (i + 1, reactionList[-1].desorb_k(), reactionList[-1].adsorb_k(), - reactionList[-1].delta_g()))
-            f = reactionList[-1].desorb_k()
-            r = reactionList[-1].adsorb_k()
-            k_forward.append(f)
-            k_reverse.append(r)
+            k_forward.append(reactionList[-1].desorb_k())
+            k_reverse.append(reactionList[-1].adsorb_k())
             if int(kmc.reactions[i][0][0]) in dict_reaction.keys():
                 dict_reaction[int(kmc.reactions[i][0][0])].append(k_forward[-1])
             else:
@@ -133,59 +177,45 @@ def add_reaction(kmc, totList, T, P, reactionList = [], k_forward = [], k_revers
 
         else:
             raise ValueError('Error: please check the reactions parameters.')
+    # Add the last step to the first one: Warming, the last reaction counted twice.
+    dict_reaction[0].append(dict_reaction[len(totList)-1][0])
+    dict_reaction_direction[0].append(dict_reaction_direction[len(totList)-1][0])
     return reactionList, k_forward, k_reverse, dict_reaction, dict_reaction_direction
 
 
 def initialize_lattice(kmc, speciesTuple):
+    """
+    initialize the lattice information: Maybe no use for SA simualtions.
+    :param kmc: Instance of Parameters
+    :param speciesTuple: Tuple to store the local minimum species information.
+    :return:
+    """
     lat = lattice.lattice(kmc.latticeSize)    # set lattice dimension.
     print('lattice size is setted to be: %d * %d' %(lat.demension[0], lat.demension[1]))
     lat.initialElements = speciesTuple[0].kind    # choose the initial kind on surface.
-    print('lattice elements is initialized to be %s' % (lat.initialElements))
+    # print('lattice elements is initialized to be %s' % (lat.initialElements))
     lat.initializeElements()    # initialize the elements on surface.
     print('lattice is initialized')
     return lat
 
 
-def main():
+def do_kmc_loop(kmc, lat, speciesTuple_kind, totList, totList_kind, dict_reaction, dict_reaction_direction,\
+        time = [0], productNum = 0, coverage_t_List = []):
+    """
+    run kMC simulation.
 
-    kmc = Config.Parameters()
-
-    # Basic parameters.
-    T = kmc.Temperature                                               # get temperature /K
-    P = {kmc.gas[i]:kmc.GasPressure[i] for i in range(len(kmc.gas))}  # get pressure    /Bar
-    latticeSize = kmc.latticeSize
-
-
-    # Input all species information.
-    localMinimum = kmc.isKindsTS.count(0)
-    test_kinds_number(kmc)
-    speciesTuple, speciesTuple_kind, TSTuple, totList, totList_kind = add_list(kmc)
-
-
-
-    # print(kmc.reactions)
-    # find out all the pathways and rate constant.
-    reactionList, k_forward, k_reverse, dict_reaction, dict_reaction_direction = add_reaction(kmc, totList, T, P)
-
-    # record the reaction direction correspond the dict_reaction
-    if len(kmc.reactionsKind) != len(kmc.reactions):
-        raise ValueError('Error: Please check the reactions number and reactionsKind number')
-
-
-    # reactionTuple = tuple(reactionList)
-    print(reactionList)
-    print(k_forward, '\n', k_reverse)
-    dict_reaction[0].append(dict_reaction[len(totList)-1][0])
-    dict_reaction_direction[0].append(dict_reaction_direction[len(totList)-1][0])
-    print(dict_reaction)
-    print(dict_reaction_direction)
-
-    # DO lattice KMC
-    lat = initialize_lattice(kmc, speciesTuple)
-
-    time = [0]                                         # initialize time
-    productNum = 0                                     # initialize the product number
-    coverage_t_List = []
+    :param kmc: Instance of Parameters
+    :param lat: lattice information
+    :param speciesTuple_kind: Tuple to store the local minimum species kinds.
+    :param totList: Tuple to store the total species information.
+    :param totList_kind: Tuple to store the total species kinds.
+    :param dict_reaction: Dict to store {IS:[k]}
+    :param dict_reaction_direction:  Dict to store {IS:[FS]}
+    :param time: initilize the start time to 0
+    :param productNum: count of the products
+    :param coverage_t_List: Add a time dependent coverage
+    :return: time, productNum, coverageList
+    """
     for i in range(len(speciesTuple_kind)):
         coverage_t_List.append(0)
     for i in range(kmc.LoopNum):                       # MC cycle number
@@ -264,17 +294,52 @@ def main():
         coverage_t_List = list(map( lambda i : coverage_t_List[i] + d_t * lat.elements[0].count(speciesTuple_kind[i])/\
                 lat.demension[0] * lat.demension[1], [ i for i in range(len(speciesTuple_kind))]))
 
-    coverageList = list(map( lambda i : i / time[-1] , coverage_t_List))
+    coverageList = list(map(lambda i: i / time[-1], coverage_t_List))
+    return time, productNum, coverageList
 
-    #print(lat.elements)
+
+def main():
+
+    kmc = Config.Parameters()
+
+    T = kmc.Temperature                                               # get temperature /K
+    P = {kmc.gas[i]:kmc.GasPressure[i] for i in range(len(kmc.gas))}  # get pressure    /Bar
+
+    # Test the input kinds and reaction number.
+    test_kinds_number(kmc)
+    test_reaction_number(kmc)
+
+    # Input all species information.
+    speciesTuple, speciesTuple_kind, TSTuple, totList, totList_kind = add_list(kmc)
+
+    # Input all reactions information.
+    reactionList, k_forward, k_reverse, dict_reaction, dict_reaction_direction\
+        = add_reaction(kmc, totList, T, P)
+
+    # Output the reactions information.
+    print("k_forward and k_reverse :", k_forward, '\n', k_reverse)
+    print("dict_reaction :", dict_reaction)
+    print("dict_reaction_direction :", dict_reaction_direction)
+
+    # Initialize the lattice information. (Not necessary for SA part)
+    lat = initialize_lattice(kmc, speciesTuple)
+
+    # DO KMC
+    time, productNum, coverageList = do_kmc_loop(kmc, lat, speciesTuple_kind, totList, totList_kind, dict_reaction, dict_reaction_direction)
+
+    # print(lat.elements)
+
+    # print coverage information, time, and product number.
     for i in range(len(speciesTuple_kind)):
         print('the coverage for species {:s} = {:.3f}'. format(speciesTuple[i].kind, coverageList[i]))
     print('kMC simulation time is %.2f' % time[-1], 'product number is %d ' % productNum)
+
+    # Output TOF.
     print('TOF calculated by product/(time * sites) by ln(r/s-1) = %.2f ' % math.log(productNum / (time[-1] * lat.demension[0]) * int(lat.demension[1])))
 
     # this method may have some problem, (1-r) should be multiple as discussed in Norskov'book: (7.4)
-#    print('TOF calculated by Theta * k_forward by ln(r/s-1) = %.2f ' \
-#          % math.log(max(coverageList) * k_forward[coverageList.index(max(coverageList))] ) )
+    print('TOF calculated by Theta * k_forward by ln(r/s-1) = %.2f ' \
+          % math.log(max(coverageList) * k_forward[coverageList.index(max(coverageList))] ) )
 
 if __name__ == "__main__":
     main()
